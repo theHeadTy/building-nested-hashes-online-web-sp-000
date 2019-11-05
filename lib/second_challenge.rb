f0fb909ceec55efa8644c94db0f069f29857020d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-05 02:05:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,21 +1,20 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+
       #your key/value pairs here!
       }, 
     :capulet => {
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      heroine_friends: []
       #your key/value pairs here!
       }}
 
   
-end
-
-describe "#second_challenge" do 
-	it "fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs" do 
-    expect(second_challenge[:montague].keys).to eq([:patriarch, :matriarch, :hero, :hero_friends])
-    expect(second_challenge[:capulet].keys).to eq([:patriarch, :matriarch, :heroine, :heroine_friends])
-    expect(second_challenge[:montague].values).to eq([{}, {}, {}, []])
-    expect(second_challenge[:capulet].values).to eq([{}, {}, {}, []])
-
-  end
 end
